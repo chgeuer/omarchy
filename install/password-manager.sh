58766e873e90971e2c56ef12b103ea2a822d1bb6
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2025-06-27 11:56:44 +0200

    Update password-manager.sh

--- install/password-manager.sh
+++ install/password-manager.sh
@@ -1,12 +1,4 @@
-if ! command -v 1password &>/dev/null; then
-  cd /tmp
-  curl -sSO https://downloads.1password.com/linux/tar/edge/x86_64/1password-latest.tar.gz
-  tar -xf 1password-latest.tar.gz
-  sudo mkdir -p /opt/1Password
-  sudo mv 1password-*/* /opt/1Password
-  sudo /opt/1Password/after-install.sh
-  rm -rf 1password*
-  cd -
+if ! command -v bitwarden-desktop &>/dev/null; then
 
-  yay -S --noconfirm --needed 1password-cli
+  pacman -S bitwarden
 fi
